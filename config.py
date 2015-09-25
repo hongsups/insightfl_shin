8b2473331f3bbf71f2c720651b8e15a5447f9b73
hongsups <hongsup.s@gmail.com>
2015-09-25 00:45:48 +0000

    ready for AWS

--- config.py
+++ config.py
@@ -8,7 +8,6 @@ class Config:
 
     # MySQL Database Settings
     MYSQL_DATABASE_HOST = 'localhost'
-    MYSQL_DATABASE_PORT = 3307
     MYSQL_DATABASE_USER = os.environ.get('MYSQL_DATABASE_USER') or None
     MYSQL_DATABASE_PASSWORD = os.environ.get('MYSQL_DATABASE_PASSWORD') or None
     MYSQL_DATABASE_DB = None
@@ -22,11 +21,13 @@ class Config:
 class DevelopmentConfig(Config):
     # Add settings for development here.
     DEBUG = True
+    MYSQL_DATABASE_PORT = 3307
 
 
 class ProductionConfig(Config):
     # Add settings for production here.
     DEBUG = False
+    MYSQL_DATABASE_PORT = 3306
     # This will be different for production:
     # MYSQL_DATABASE_HOST = 'localhost'
 
